073986f04848db4056fa80f666135107d2ce3cb0
bauerbrun0 <bauerbruno@protonmail.com>
2026-07-08 23:08:21 +0200

    chore: add tailwind to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://brun0.codes"
+  site: "https://brun0.codes",
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });

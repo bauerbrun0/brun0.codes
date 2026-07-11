13240471e06c5b657e038799bcbde50e28c828ca
bauerbrun0 <bauerbruno@protonmail.com>
2026-07-11 18:42:33 +0200

    feat: add custom 404 page

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -18,6 +18,9 @@ export const ui = {
     "hero.name": "Brúnó Bauer",
     "hero.picture-alt": "A picture of me.",
     "hero.job": "Full-stack developer",
+    "404.title": "404 - Page Not Found",
+    "404.description": "Sorry, the page you are looking for does not exist.",
+    "404.back": "Go back to the homepage",
   },
   hu: {
     "title.home": "brun0.codes",
@@ -29,6 +32,9 @@ export const ui = {
     "hero.name": "Bauer Brúnó",
     "hero.picture-alt": "Egy kép rólam.",
     "hero.job": "Full-stack fejlesztő",
+    "404.title": "404 - Oldal nem található",
+    "404.description": "A keresett oldal nem létezik.",
+    "404.back": "Vissza a kezdőlapra",
   },
 } as const;
 

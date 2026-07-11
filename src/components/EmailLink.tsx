4868813b3e2ec160cc13fd7c668ba7402c0f101a
bauerbrun0 <bauerbruno@protonmail.com>
2026-07-11 17:32:06 +0200

    feat: use client-side rendering for email link to deter scrapers

---
+++ src/components/EmailLink.tsx
@@ -0,0 +1,15 @@
+export function EmailLink() {
+  const user = "bauerbruno"
+  const domain = "protonmail"
+  const tld = "com"
+
+  return (
+    <a
+        className="w-fit font-medium underline text-gray-800 dark:text-gray-200 dark:hover:text-gray-400 hover:text-gray-600"
+        href={`mailto:${user}@${domain}.${tld}`}
+        target="_blank"
+    >
+      {`${user} [at] ${domain} [dot] ${tld}`}
+    </a>
+  );
+}
